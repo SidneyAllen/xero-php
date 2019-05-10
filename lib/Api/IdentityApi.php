5c3cbd9d89a0cb360302ab6dcebe68f697324540
SidneyAllen <sid.maestre@gmail.com>
2019-05-10 07:59:43 -0700

    changed token object to post as JSON  body

--- lib/Api/IdentityApi.php
+++ lib/Api/IdentityApi.php
@@ -339,7 +339,297 @@ class IdentityApi
         $query = \GuzzleHttp\Psr7\build_query($queryParams);
         return new Request(
             'GET',
-            "https://api.xero.com/" . $resourcePath . ($query ? "?{$query}" : ''),
+            "https://api.xero.com" . $resourcePath . ($query ? "?{$query}" : ''),
+            $headers,
+            $httpBody
+        );
+    }
+
+
+    /**
+     * Operation refreshToken
+     *
+     * Allows you to refresh your access token
+     *
+     * @param  \SidneyAllen\XeroPHP\Models\Identity\Token $token form data to post during a token refresh (required)
+     * @param  string $grant_type the grant type for token (optional)
+     * @param  string $content_type the content type to be returned (application/json) (optional)
+     *
+     * @throws \SidneyAllen\XeroPHP\ApiException on non-2xx response
+     * @throws \InvalidArgumentException
+     * @return \SidneyAllen\XeroPHP\Models\Identity\Connection[]
+     */
+    public function refreshToken($token, $grant_type = null, $content_type = null)
+    {
+        list($response) = $this->refreshTokenWithHttpInfo($token, $grant_type, $content_type);
+        return $response;
+    }
+
+    /**
+     * Operation refreshTokenWithHttpInfo
+     *
+     * Allows you to refresh your access token
+     *
+     * @param  \SidneyAllen\XeroPHP\Models\Identity\Token $token form data to post during a token refresh (required)
+     * @param  string $grant_type the grant type for token (optional)
+     * @param  string $content_type the content type to be returned (application/json) (optional)
+     *
+     * @throws \SidneyAllen\XeroPHP\ApiException on non-2xx response
+     * @throws \InvalidArgumentException
+     * @return array of \SidneyAllen\XeroPHP\Models\Identity\Connection[], HTTP status code, HTTP response headers (array of strings)
+     */
+    public function refreshTokenWithHttpInfo($token, $grant_type = null, $content_type = null)
+    {
+        $request = $this->refreshTokenRequest($token, $grant_type, $content_type);
+
+        try {
+            $options = $this->createHttpClientOption();
+            try {
+                $response = $this->client->send($request, $options);
+            } catch (RequestException $e) {
+                throw new ApiException(
+                    "[{$e->getCode()}] {$e->getMessage()}",
+                    $e->getCode(),
+                    $e->getResponse() ? $e->getResponse()->getHeaders() : null,
+                    $e->getResponse() ? $e->getResponse()->getBody()->getContents() : null
+                );
+            }
+
+            $statusCode = $response->getStatusCode();
+
+            if ($statusCode < 200 || $statusCode > 299) {
+                throw new ApiException(
+                    sprintf(
+                        '[%d] Error connecting to the API (%s)',
+                        $statusCode,
+                        $request->getUri()
+                    ),
+                    $statusCode,
+                    $response->getHeaders(),
+                    $response->getBody()
+                );
+            }
+
+            $responseBody = $response->getBody();
+            switch($statusCode) {
+                case 200:
+                    if ('\SidneyAllen\XeroPHP\Models\Identity\Connection[]' === '\SplFileObject') {
+                        $content = $responseBody; //stream goes to serializer
+                    } else {
+                        $content = $responseBody->getContents();
+                    }
+
+                    return [
+                        ObjectSerializer::deserialize($content, '\SidneyAllen\XeroPHP\Models\Identity\Connection[]', []),
+                        $response->getStatusCode(),
+                        $response->getHeaders()
+                    ];
+            }
+
+            $returnType = '\SidneyAllen\XeroPHP\Models\Identity\Connection[]';
+            $responseBody = $response->getBody();
+            if ($returnType === '\SplFileObject') {
+                $content = $responseBody; //stream goes to serializer
+            } else {
+                $content = $responseBody->getContents();
+            }
+
+            return [
+                ObjectSerializer::deserialize($content, $returnType, []),
+                $response->getStatusCode(),
+                $response->getHeaders()
+            ];
+
+        } catch (ApiException $e) {
+            switch ($e->getCode()) {
+                case 200:
+                    $data = ObjectSerializer::deserialize(
+                        $e->getResponseBody(),
+                        '\SidneyAllen\XeroPHP\Models\Identity\Connection[]',
+                        $e->getResponseHeaders()
+                    );
+                    $e->setResponseObject($data);
+                    break;
+            }
+            throw $e;
+        }
+    }
+
+    /**
+     * Operation refreshTokenAsync
+     *
+     * Allows you to refresh your access token
+     *
+     * @param  \SidneyAllen\XeroPHP\Models\Identity\Token $token form data to post during a token refresh (required)
+     * @param  string $grant_type the grant type for token (optional)
+     * @param  string $content_type the content type to be returned (application/json) (optional)
+     *
+     * @throws \InvalidArgumentException
+     * @return \GuzzleHttp\Promise\PromiseInterface
+     */
+    public function refreshTokenAsync($token, $grant_type = null, $content_type = null)
+    {
+        return $this->refreshTokenAsyncWithHttpInfo($token, $grant_type, $content_type)
+            ->then(
+                function ($response) {
+                    return $response[0];
+                }
+            );
+    }
+
+    /**
+     * Operation refreshTokenAsyncWithHttpInfo
+     *
+     * Allows you to refresh your access token
+     *
+     * @param  \SidneyAllen\XeroPHP\Models\Identity\Token $token form data to post during a token refresh (required)
+     * @param  string $grant_type the grant type for token (optional)
+     * @param  string $content_type the content type to be returned (application/json) (optional)
+     *
+     * @throws \InvalidArgumentException
+     * @return \GuzzleHttp\Promise\PromiseInterface
+     */
+    public function refreshTokenAsyncWithHttpInfo($token, $grant_type = null, $content_type = null)
+    {
+        $returnType = '\SidneyAllen\XeroPHP\Models\Identity\Connection[]';
+        $request = $this->refreshTokenRequest($token, $grant_type, $content_type);
+
+        return $this->client
+            ->sendAsync($request, $this->createHttpClientOption())
+            ->then(
+                function ($response) use ($returnType) {
+                    $responseBody = $response->getBody();
+                    if ($returnType === '\SplFileObject') {
+                        $content = $responseBody; //stream goes to serializer
+                    } else {
+                        $content = $responseBody->getContents();
+                    }
+
+                    return [
+                        ObjectSerializer::deserialize($content, $returnType, []),
+                        $response->getStatusCode(),
+                        $response->getHeaders()
+                    ];
+                },
+                function ($exception) {
+                    $response = $exception->getResponse();
+                    $statusCode = $response->getStatusCode();
+                    throw new ApiException(
+                        sprintf(
+                            '[%d] Error connecting to the API (%s)',
+                            $statusCode,
+                            $exception->getRequest()->getUri()
+                        ),
+                        $statusCode,
+                        $response->getHeaders(),
+                        $response->getBody()
+                    );
+                }
+            );
+    }
+
+    /**
+     * Create request for operation 'refreshToken'
+     *
+     * @param  \SidneyAllen\XeroPHP\Models\Identity\Token $token form data to post during a token refresh (required)
+     * @param  string $grant_type the grant type for token (optional)
+     * @param  string $content_type the content type to be returned (application/json) (optional)
+     *
+     * @throws \InvalidArgumentException
+     * @return \GuzzleHttp\Psr7\Request
+     */
+    protected function refreshTokenRequest($token, $grant_type = null, $content_type = null)
+    {
+        // verify the required parameter 'token' is set
+        if ($token === null || (is_array($token) && count($token) === 0)) {
+            throw new \InvalidArgumentException(
+                'Missing the required parameter $token when calling refreshToken'
+            );
+        }
+
+        $resourcePath = '/connect/token';
+        $formParams = [];
+        $queryParams = [];
+        $headerParams = [];
+        $httpBody = '';
+        $multipart = false;
+
+        // header params
+        if ($grant_type !== null) {
+            $headerParams['grant-type'] = ObjectSerializer::toHeaderValue($grant_type);
+        }
+        // header params
+        if ($content_type !== null) {
+            $headerParams['Content-Type'] = ObjectSerializer::toHeaderValue($content_type);
+        }
+
+
+        // body params
+        $_tempBody = null;
+        if (isset($token)) {
+            $_tempBody = $token;
+        }
+
+        if ($multipart) {
+            $headers = $this->headerSelector->selectHeadersForMultipart(
+                ['application/json']
+            );
+        } else {
+            $headers = $this->headerSelector->selectHeaders(
+                ['application/json'],
+                ['application/json']
+            );
+        }
+
+        // for model (json/xml)
+        if (isset($_tempBody)) {
+            // $_tempBody is the method argument, if present
+            if ($headers['Content-Type'] === 'application/json') {
+                $httpBody = \GuzzleHttp\json_encode(ObjectSerializer::sanitizeForSerialization($_tempBody));
+            } else {
+                $httpBody = $_tempBody;
+            }
+        } elseif (count($formParams) > 0) {
+            if ($multipart) {
+                $multipartContents = [];
+                foreach ($formParams as $formParamName => $formParamValue) {
+                    $multipartContents[] = [
+                        'name' => $formParamName,
+                        'contents' => $formParamValue
+                    ];
+                }
+                // for HTTP post (form)
+                $httpBody = new MultipartStream($multipartContents);
+
+            } elseif ($headers['Content-Type'] === 'application/json') {
+                $httpBody = \GuzzleHttp\json_encode($formParams);
+
+            } else {
+                // for HTTP post (form)
+                $httpBody = \GuzzleHttp\Psr7\build_query($formParams);
+            }
+        }
+
+        // this endpoint requires OAuth (access token)
+        if ($this->config->getAccessToken() !== null) {
+            $headers['Authorization'] = 'Bearer ' . $this->config->getAccessToken();
+        }
+
+        $defaultHeaders = [];
+        if ($this->config->getUserAgent()) {
+            $defaultHeaders['User-Agent'] = $this->config->getUserAgent();
+        }
+
+        $headers = array_merge(
+            $defaultHeaders,
+            $headerParams,
+            $headers
+        );
+
+        $query = \GuzzleHttp\Psr7\build_query($queryParams);
+        return new Request(
+            'POST',
+            "https://identity.xero.com" . $resourcePath . ($query ? "?{$query}" : ''),
             $headers,
             $httpBody
         );
